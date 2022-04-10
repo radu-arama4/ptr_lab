17e3a6fbca082f048d09fd77725093a7362a15a9
radu-arama4 <rarama33@gmail.com>
2022-04-10 22:42:57 +0300

    fix: improved search of pid of sibling

--- lib/tweet_processer/util/siblings_accesor.ex
+++ lib/tweet_processer/util/siblings_accesor.ex
@@ -1,16 +1,8 @@
 defmodule TweetProcesser.SiblingsAccesor do
   def get_sibling(parent_pid, type) do
     children = Supervisor.which_children(parent_pid)
-
-    for child <- children do
-      {child_type, pid, _ceva, _ceva2} = child
-      IO.puts "iteration!!"
-      IO.inspect child_type
-      if child_type == type do
-        right_pid = pid
-        {right_pid}
-      end
-    end
+    {_child_type, pid, _ceva, _ceva2} = children |> List.keyfind(type, 0)
+    {pid}
   end
 end
 
